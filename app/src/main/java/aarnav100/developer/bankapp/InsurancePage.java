e37da9dafe8e5815a50e701160738778f6c97b13
devaar100 <developer.aarnav100@gmail.com>
2017-07-24 15:42:04 +0530

    errors fixed

--- app/src/main/java/aarnav100/developer/bankapp/InsurancePage.java
+++ app/src/main/java/aarnav100/developer/bankapp/InsurancePage.java
@@ -1,5 +1,6 @@
 package aarnav100.developer.bankapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,18 +81,15 @@ public class InsurancePage extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_login) {
+            Intent i=new Intent(this,Login.class);
+            startActivity(i);
+        } else if (id == R.id.nav_sug) {
+            Intent i=new Intent(this,Suggestion.class);
+            startActivity(i);
+        } else if (id == R.id.nav_map) {
+            Intent i=new Intent(this,MapsActivity.class);
+            startActivity(i);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
